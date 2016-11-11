32b0b1f420e2b77c39425d28a51ba563d62cbd6c
MATHWORKS\jongguk <huhahaza@gmail.com>
2016-11-11 16:37:56 +0900

    first commit : inisiate Git this folder

---
+++ contact manu.py
@@ -0,0 +1,13 @@
+def print_menu():
+    print("1. 연락처 입력")
+    print("2. 연락처 출력")
+    print("3. 연락처 삭제")
+    print("4. 종료")
+    menu = input("메뉴선택: ")
+    return int(menu)
+
+def main():
+    while 1:
+        menu = print_menu()
+        if menu == 4:
+            break
---
+++ with COM.py
@@ -0,0 +1,26 @@
+import sys
+from PyQt5.QtWidgets import *
+'''
+app = QApplication(sys.argv)
+label = QLabel("Hello PyQt")
+label.show()
+app.exec_()
+'''
+'''
+app = QApplication(sys.argv)
+print(sys.argv)
+button = QPushButton("Quit")
+button.show()
+app.exec_()
+'''
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("PyStock")
+        self.setGeometry(300, 300, 300, 400)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWindow = MyWindow()
+    myWindow.show()
+    app.exec_()
